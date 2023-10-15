0ea9f226c74713e193db5649589ec7def6bf1328
Kingron <kingron.lu@gmail.com>
2023-10-15 14:41:41 +0800

    Update aad.js

--- aad.js
+++ aad.js
@@ -17,11 +17,12 @@
                 for (var addedNode of mutation.addedNodes) {
                     // 检查是否是 <script> 元素
                     if (addedNode.tagName === 'SCRIPT') {
-                        if (addedNode.src.indexOf('adsninja_client.js' > 0)) {
+                        if (addedNode.src.indexOf('adsninja_client.js') > 0) {
                             addedNode.src = "";
+                        } else {
+                            addedNode.textContent = addedNode.textContent.replace("r9aeadS();", "");
+                            addedNode.textContent = addedNode.textContent.replace("document.getElementById('google_esf')", "document.body");
                         }
-                        addedNode.textContent = addedNode.textContent.replace("r9aeadS();", "");
-                        addedNode.textContent = addedNode.textContent.replace("document.getElementById('google_esf')", "document.body");
                     }
                 }
             }
